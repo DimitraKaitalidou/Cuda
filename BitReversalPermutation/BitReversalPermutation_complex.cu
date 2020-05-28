6af7d224b3a5d84e954db5d438a7ef0ad859b8db
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-05-28 21:01:52 +0300

    [MODIFIED] removed spaces and brackets

--- BitReversalPermutation/BitReversalPermutation_complex.cu
+++ BitReversalPermutation/BitReversalPermutation_complex.cu
@@ -9,21 +9,21 @@
 #define THR_PER_BL 8
 #define BL_PER_GR 32
 
-__global__ void kernel1(int* D, int* Q, int k) {
+__global__ void kernel1(int* D, int* Q, int k){
 
    // Find index
    int i = blockIdx.x * blockDim.x + threadIdx.x;
    int block = (int)(i / (2 * k));
    int j;
 
-   if (i % 2 == 0) { j = 2 * block * k + (int)(i / 2) - k * ((int)(i / (2 * k))); }
-   else { j = (2 * block + 1) * k + (int)(i / 2) - k * ((int)(i / (2 * k))); }
+   if(i % 2 == 0) j = 2 * block * k + (int)(i / 2) - k * ((int)(i / (2 * k)));
+   else j = (2 * block + 1) * k + (int)(i / 2) - k * ((int)(i / (2 * k)));
 
    // Assign the values to the output array
    Q[j] = D[i];
 }
 
-__global__ void kernel2(int* D, int* Q) {
+__global__ void kernel2(int* D, int* Q){
 
    int i = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -42,14 +42,14 @@ int main(int argc, char** argv){
 
    // Allocate the memory on the CPU
    D = (int*)malloc(N * sizeof(int));
-   if (D == NULL) { printf("Allocation failure"); }
+   if(D == NULL) printf("Allocation failure");
 
    // Allocate the memory on the CPU
    Q = (int*)malloc(N * sizeof(int));
-   if (Q == NULL) { printf("Allocation failure"); }
+   if(Q == NULL) printf("Allocation failure");
 
    // printf("INPUT\n");
-   for (int i = 0; i < N; i++)
+   for(int i = 0; i < N; i++)
       {
          D[i] = rand() % 100;
          Q[i] = 0;
@@ -57,7 +57,7 @@ int main(int argc, char** argv){
          // Print the initial values - optional
          // printf("The number %d is: ", i);
          // printf("%d \n", D[i]);
-   }
+      }
 
    // Allocate memory in the GPU
    cudaMalloc((void**)&dev_d, N * sizeof(int));
@@ -71,7 +71,7 @@ int main(int argc, char** argv){
    dim3 dimBlock(THR_PER_BL, 1);
    dim3 dimGrid(BL_PER_GR, 1);
 
-   for (int i = 1; i < bits; i++)
+   for(int i = 1; i < bits; i++)
       {
          kernel1 << <dimGrid, dimBlock >> > (dev_d, dev_q, N / (int)(pow(2.0, (double)i)));
          kernel2 << <dimGrid, dimBlock >> > (dev_d, dev_q);
@@ -83,7 +83,7 @@ int main(int argc, char** argv){
 
    /* Print results - optional
    printf("RESULT\n");
-   for (int i = 0; i < N; i++)
+   for(int i = 0; i < N; i++)
       {
          // Print the final values - optional
          printf("The number %d is: ", i);
