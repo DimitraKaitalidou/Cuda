6f1d4fec49219d0ccc57bcf5ac246c9aedec60c0
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-05-28 21:14:52 +0300

    [MODIFIED] removed spaces and brackets

--- Floyd-Warshall/FloydWarshall_simple.cu
+++ Floyd-Warshall/FloydWarshall_simple.cu
@@ -8,7 +8,7 @@
 #define THR_PER_BL 8
 #define BL_PER_GR 32
 
-__global__ void kernel(int* D, int* q, int k) {
+__global__ void kernel(int* D, int* q, int k){
 
    // Find index of i row and j column of the distance array
    int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -32,16 +32,16 @@ int main(int argc, char** argv){
 
    // Allocate the memory on the CPU
    D = (int**)malloc(N * sizeof(int*));
-   if (D == NULL) { printf("Allocation failure"); }
+   if(D == NULL) printf("Allocation failure");
    for(int i = 0; i < N; i++)
       {
          D[i] = (int*)malloc(N * sizeof(int));
-         if(D[i] == NULL) { printf("Allocation failure"); }
+         if(D[i] == NULL) printf("Allocation failure");
       }
    d = (int*)malloc(N * N * sizeof(int));
-   if(d == NULL) { printf("Allocation failure"); }
+   if(d == NULL) printf("Allocation failure");
    q = (int*)malloc(N * N * sizeof(int));
-   if(q == NULL) { printf("Allocation failure"); }
+   if(q == NULL) printf("Allocation failure");
 
    // Fill the arrays 'd' on the CPU
    for(int i = 0; i < N; i++)
@@ -84,7 +84,7 @@ int main(int argc, char** argv){
    dim3 dimBlock(THR_PER_BL, THR_PER_BL);
    dim3 dimGrid(BL_PER_GR, BL_PER_GR);
 
-   for(int k = 0; k<N; k++)
+   for(int k = 0; k < N; k++)
       {
          kernel << <dimGrid, dimBlock >> >(dev_d, dev_q, k);
       }
